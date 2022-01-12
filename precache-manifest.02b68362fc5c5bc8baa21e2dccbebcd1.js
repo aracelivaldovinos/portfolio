392693800f901b17f5dff9c307fae9909c6c2aa8
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2022-01-12 16:53:14 -0600

    Updates

---
+++ precache-manifest.02b68362fc5c5bc8baa21e2dccbebcd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1936579a5fb90761d1bff5ac084fef3c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d66dec3bf48055e08cf6",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2ac1928591d67cfdeea0",
+    "url": "/portfolio/static/css/main.fc97ecf1.chunk.css"
+  },
+  {
+    "revision": "d66dec3bf48055e08cf6",
+    "url": "/portfolio/static/js/2.3f46efb0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.3f46efb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac1928591d67cfdeea0",
+    "url": "/portfolio/static/js/main.8cfad57d.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
